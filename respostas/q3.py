9fb92d79e71de2239b9f8976d56c866c4ea25166
Ruan-Carlos-dev <ruancarlosdejesus0@gmail.com>
2025-04-08 21:06:58 -0300

    Update q3.py Nome, Curso, Faculdade

--- respostas/q3.py
+++ respostas/q3.py
@@ -1,1 +1,4 @@
 #Crie um programa que exiba as seguintes informações, cada uma em uma linha:
+print("Nome:Ruan Carlos")
+print("Curso:Lincenciatura em Computação")
+print("Faculdade: IFPI - Zona Sul")
